3b0fc88845fe29b80ba73a011a5700624bf17d45
willis20188 <83527164+willis20188@users.noreply.github.com>
2026-01-13 13:01:09 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,13 +1,14 @@
 <?php
 /**
- * JustNews Child Theme functions
+ * JuestNews Child Theme functions
  */
 
 add_action( 'wp_enqueue_scripts', function () {
     wp_enqueue_style(
-        'newsjust-child-style',
+        'juestnews-child-style',
         get_stylesheet_uri(),
-        [ 'newsjust-style' ],
+        [],
         wp_get_theme()->get('Version')
     );
-});
+}, 20 );
+
